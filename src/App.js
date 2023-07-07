e0effb1e94409622b51009e6cef790dfb1b07451
Longdh <danghoanglong511@gmail.com>
2023-07-07 15:34:06 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <Fragment>
       <header className="header flex justify-center items-center gap-x-5 text-white py-10 mb-10">
-        <span className="text-primary">Home</span>
+        <span className="text-primary">Home1</span>
         <span>Movies</span>
       </header>
       <section className="banner h-[500px] page-container mb-20">
